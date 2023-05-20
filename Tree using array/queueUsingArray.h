674309806a2c90af0c9b438c2cca20016f55d75a
alittlebitofit <42311431+alittlebitofit@users.noreply.github.com>
2023-05-20 17:10:29 +0530

    Add files via upload

---
+++ Tree using array/queueUsingArray.h
@@ -0,0 +1,61 @@
+﻿class Node {
+	private:
+		Node *lchild = NULL;
+		int data;
+		Node *rchild =  NULL;
+	
+	public:
+		Node() {}
+		
+		Node(int x) {this->data = x;}
+		
+		Node *getLchild() {return this->lchild;}
+		void setLchild(Node *lchild) {this->lchild=lchild;}
+		
+		int getData() {return this->data;}
+		void setData(int x) {this->data=x;}
+		
+		Node *getRchild() {return this->rchild;}
+		void setRchild(Node *rchild) {this->rchild=rchild;}
+	
+};
+
+class Queue {
+	private:
+		int size;
+		int front = -1;
+		int rear = -1;
+		Node * *Q;
+	
+	public:
+	
+		Queue() {
+			this->size = 100;
+			this->Q = new Node*[this->size];
+		}
+		
+		void enqueue(Node *t) {
+			if(!this->isFull()) {
+				
+				Q[++this->rear] = t;
+			} else {
+				cout<<"Queue overflow\n";
+			}
+		}
+		
+		Node * dequeue() {
+			if(!this->isEmpty()) {
+				return Q[++this->front];
+			}
+			return NULL;
+		}
+		
+		bool isEmpty() {
+			return this->front ==  this->rear;
+		}
+		
+		bool isFull() {
+			return this->rear == (this->size - 1);
+		}
+	
+};
